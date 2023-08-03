cea263eacfe9387cb326f1be2bad6e017b3a2ed7
Wesley Souza Roman <wesley.sdesouza@gmail.com>
2023-08-03 11:28:10 -0300

    Obtendo um usuário por Id

--- src/main/java/com/wsouza/workshopmongo/services/UserService.java
+++ src/main/java/com/wsouza/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.wsouza.workshopmongo.domain.User;
 import com.wsouza.workshopmongo.repository.UserRepository;
+import com.wsouza.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +17,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 }
