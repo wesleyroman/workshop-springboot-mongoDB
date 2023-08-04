db11d854650f9658dfd1f1922a93797cecb71771
Wesley Souza Roman <wesley.sdesouza@gmail.com>
2023-08-04 14:37:26 -0300

    Obtendo um post por id

---
+++ src/main/java/com/wsouza/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.wsouza.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wsouza.workshopmongo.domain.Post;
+import com.wsouza.workshopmongo.repository.PostRepository;
+import com.wsouza.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+	
+}
