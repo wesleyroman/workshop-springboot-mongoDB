66f605d41e2ce5fa0399b5c70dbbc3718b617e8e
Wesley Souza Roman <wesley.sdesouza@gmail.com>
2023-08-07 16:05:43 -0300

    Conulsta com vários critérios

--- src/main/java/com/wsouza/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/wsouza/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.wsouza.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,5 +15,8 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ {date: {$gte: ?1}}, {date:{$lte: ?2}}, { $or: [{'title': { $regex: ?0, $options: 'i' }},{'body': { $regex: ?0, $options: 'i' }},{'comments.text': { $regex: ?0, $options: 'i' }}] } ] }")
+	List<Post> fullSearch(String tex, Date minDate, Date maxDate);
 
 }
--- src/main/java/com/wsouza/workshopmongo/services/PostService.java
+++ src/main/java/com/wsouza/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.wsouza.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,11 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repo.findByTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 	
 }
